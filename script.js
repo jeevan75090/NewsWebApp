e4cbbb871a0eb75adc0e8c2b978300c919578bba
Jeevan Panwar <111672721+jeevan75090@users.noreply.github.com>
2023-07-10 19:28:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
-const url = "https://newsapi.org/v2/everything?q=";
+const url = "http://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
 
